c7162cdad3886066bd9e435ddb7bc733f48548a9
Andres <98991190+Andres1555@users.noreply.github.com>
2025-07-29 00:20:58 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# proyecto_innovacion_backend
+## Pasos para correr el programa
+
+1- Instalar dependencias
+- npm i
+
+2- Correr el main
+- npm start
+
+## Tecnologias usadas
+
+- Express.js
+- SQLITE
+- Nodes.js
+
+## Programas nesecarios para correr el proyecto
+
+-Nodes.js
+
---
+++ src/tesis/routes.js
@@ -0,0 +1,40 @@
+import express from "express";
+import multer from "multer";
+import {
+    getTesis,
+    getTesisById,
+    uploadTesis,
+    downloadTesis,
+    deleteTesis,
+    updateTesis,
+    getTesisByName,
+    digitalizetesis
+} from "./controllers.js"; // Asegúrate de que la ruta es correcta
+
+const router = express.Router();
+
+// Configuración de `multer` para la subida de archivos
+const storage = multer.diskStorage({
+    destination: "uploads/",
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    }
+});
+
+const upload = multer({
+    storage,
+    limits: { fileSize: 64 * 1024 * 1024 }, // 10 MB de tamaño máximo
+});
+
+// Definición de rutas
+router.get("/tesis", getTesis); // Obtener todas las tesis
+router.get("/tesis/cadena/:nombre", getTesisByName); // Obtener una tesis que contenga x cadena en su nombre
+router.get("/tesis/:id", getTesisById); // Obtener una tesis por ID
+router.post("/tesis", upload.single("archivo_pdf"), uploadTesis); // Subir una nueva tesis con PDF
+router.get("/tesis/:id/download", downloadTesis); // Descargar un PDF de una tesis
+router.delete("/tesis/:id", deleteTesis); // Eliminar una tesis
+router.post("/tesis/digital", upload.single("archivo"), digitalizetesis);
+// Ruta que maneja la carga del archivo
+router.put('/tesis/:id', upload.single('archivo_pdf'), updateTesis);
+
+export default router;
