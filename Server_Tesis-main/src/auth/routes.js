589a6cd6f6795054d2adcc16c92e3167967e23ab
Andres <98991190+Andres1555@users.noreply.github.com>
2025-07-29 00:15:37 -0400

    Delete Server_Tesis-main directory

--- Server_Tesis-main/src/auth/routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { postlogincontroller  } from './controllers.js';
-
-const router = express.Router();
-
-router.post('/', postlogincontroller);
-
-
-export default router;
